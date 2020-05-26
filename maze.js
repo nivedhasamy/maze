f7fc44ab4d3a5d4af888dbbaa549473534c759f6
NihilisticNive25 <nivedhasamy@gmail.com>
2020-05-26 22:35:04 +0530

    ramdomly generate maze path

--- maze.js
+++ maze.js
@@ -4,7 +4,7 @@ var w = 40;
 var height = 400;
 var width = 400;
 var grid = [];
-
+var current;
 
 function setup() {
     let maze = document.getElementById('maze');
@@ -19,9 +19,8 @@ function setup() {
         }
     }
 
-    for (let i = 0; i < grid.length; i++) {
-        grid[i].show();
-    }
+    current = grid[0];
+
 }
 
 function draw() {
@@ -30,11 +29,33 @@ function draw() {
         ctx = maze.getContext('2d');
         ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
         ctx.fillRect(0, 0, width, height);
-        setup()
+        setup();
+
+        current.visited = true;
+       
+        while (1) {
+            current.show();
+            next = current.checkNeighbours();
+
+            if (!next) {
+                break;
+            }
+
+            next.visited = true;
+            current = next;
+        }
+
+        for (let i = 0; i < grid.length; i++) {
+            grid[i].show();
+        }
 
     } else {
         // canvas-unsupported code here
     }
+}
+
+function getIndex(i, j) {
+    return (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) ? 0 : i + j * cols
 }
 
 function Cell(i, j) {
@@ -42,10 +63,12 @@ function Cell(i, j) {
     this.i = i;
     this.j = j;
     this.walls = [true, true, true, true];
+    this.visited = false;
 
     this.show = function() {
         var x = this.i * w;
         var y = this.j * w;
+
         //top
         if (this.walls[0]) {
             ctx.moveTo(x, y);
@@ -66,14 +89,51 @@ function Cell(i, j) {
             ctx.moveTo(x, y + w);
             ctx.lineTo(x, y);
         }
+
         ctx.strokeStyle = "#fff";
         ctx.stroke();
 
         //draw small rectangles
-        // ctx.beginPath();
-        // ctx.rect(x,y,w-1,w-1);
-        // ctx.closePath();
-        // ctx.fill()
+        if (this.visited) {
+            // ctx.beginPath();
+            ctx.rect(x, y, w - 1, w - 1);
+            //ctx.closePath();
+            ctx.fill()
+        }
+
+    }
+
+
+
+    this.checkNeighbours = function() {
+        var neighbours = [];
+
+        var top = grid[getIndex(i, j - 1)];
+        var right = grid[getIndex(i + 1, j)];
+        var bottom = grid[getIndex(i, j + 1)];
+        var left = grid[getIndex(i - 1, j)];
+
+        if (top && !top.visited) {
+            neighbours.push(top)
+        }
+        if (right && !right.visited) {
+            neighbours.push(right)
+        }
+        if (bottom && !bottom.visited) {
+            neighbours.push(bottom)
+        }
+
+        if (left && !left.visited) {
+            neighbours.push(left)
+        }
+        if (neighbours.length > 0) {
+            var r = Math.floor(Math.random() * (neighbours.length - 0)) + 0
+            return neighbours[r]
+        } else {
+            return undefined;
+        }
+
+        console.log({ neighbours })
     }
 
 
